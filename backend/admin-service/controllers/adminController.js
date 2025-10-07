aa8fe77442879828e6c829059cb3b78a0ed6cfa1
eferrel <epferrell21@gmail.com>
2025-10-07 12:38:00 -0400

    Added empty function def to postEvents in adminController to fix error when running server.js

--- backend/admin-service/controllers/adminController.js
+++ backend/admin-service/controllers/adminController.js
@@ -4,7 +4,13 @@
 
 const { getEvents } = require('../models/adminModel');
 const listEvents = (req, res) => {
-const events = getEvents();
-res.json(events);
+    const events = getEvents();
+    res.json(events);
 };
-module.exports = { listEvents };
+
+const postEvents = (req, res) => {
+
+    // Need to fill this out, (req, res) part might be wrong too, just making it into
+    // a function to hopefully fix a bug
+}
+module.exports = { listEvents, postEvents };
