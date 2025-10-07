9abe27c6df615f3287844eeab06b90945da6d17d
eferrel <epferrell21@gmail.com>
2025-10-07 00:40:22 -0400

    Tried to start figuring out the post api stuff

--- backend/admin-service/server.js
+++ backend/admin-service/server.js
@@ -9,6 +9,25 @@ const routes = require('./routes/adminRoutes');
 app.use(cors());
 app.use('/api', routes);
 
+// New
+const res = require("node_modules/express/lib/response")
+
+app.post("/../shared-db/database.sqlite", (req, res) => {
+
+    try {
+        console.log(req.body.event);
+        return res.json({
+            status: 200, 
+            success: true;
+        });
+    } catch (error) {
+        return res.json ({
+            status: 400,
+            success: false,
+        });
+}});
+
+
 // Admin-service runs on port 5001
 const PORT = 5001;
 
