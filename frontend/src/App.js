5d19f847d182e1b5e94c31d5930d9d0f75bd061e
StellaHerzberg <157054101+StellaHerzberg@users.noreply.github.com>
2025-10-10 18:28:50 -0400

    Update comments App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+// File defines the main React component for the Tiger Tix frontend application. Retrieves event data from the backend
+// API and provides functionality to retrieve event data, purchase tickets, etc.
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+// Serves as root componenet of web app. Fetches the event data, displays events, handles user interactions.
+// Returns: the webpage displaying all available events and buttons
+// Side effects: indicates network request on component mount to get event data and update state when 
+// necessary.
 function App() {
   const [events, setEvents] = useState([]);
+  
+  // Gets event data from API when it first mounts. Ensures list is populated when page loads.
+  // Sends request to backend and updates local state
   useEffect(() => {
     fetch('http://localhost:6001/api/events')
     .then((res) => res.json())
@@ -11,26 +19,35 @@ function App() {
     .catch((err) => console.error(err));
   }, [])
 
+  //Handles process of purchasing tocker for a given event. Sends request to API to decrement available
+  // ticket count, etc.
+  // Params: eventName - name of event that a ticket is being purchased for
+  // Params: id - unique ID of event being purchased
+  // Returns: Promise<void> - operations to update database
   const buyTicket = async (eventName, id) => {
 
+    // Send request
     try {
       const res = await fetch(`http://localhost:6001/api/events/${id}/purchase`, {
         method: 'POST'
       });
 
       const data = await res.json();
-
+      
+      // Handles errors
       if (!res.ok) {
         alert(`Error: ${data.error}`);
         return;
       }
 
+      //Update ticker count for purchase
       setEvents(prevEvents =>
         prevEvents.map(event =>
           event.id === id ? { ...event, numTickets: data.ticketsRemaining} : event
         )
       );
 
+      //Notify user
       alert(`Ticket purchased for: ${eventName}. Remaining tickets: ${data.ticketsRemaining}`);
 
 
@@ -41,7 +58,7 @@ function App() {
     }
   };
 
-
+  //Displays page title, list of events with all the event information plus operations
   return (
     <div className="App">
       <h1 role="webpage_title">Clemson Campus Events</h1>
