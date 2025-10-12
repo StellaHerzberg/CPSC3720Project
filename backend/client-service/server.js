4f3209eff59bd3c65c3c21ec03f3060055bc97b0
StellaHerzberg <157054101+StellaHerzberg@users.noreply.github.com>
2025-10-12 10:56:50 -0500

    Update server.js commetns

--- backend/client-service/server.js
+++ backend/client-service/server.js
@@ -1,3 +1,6 @@
+// Server file initializes the Express application for the client-facing API. Handles incoming
+// requests from frontend then handles it accordingly to mount all routes defined in clientRoutes.
+// Client-service allows users to view events and purchase tickets.
 
 // WILL NEED TO CHANGE, COPIED BASIC SERVER FROM PROVIDED TEMPLATE
 // CLIENT SERVER
