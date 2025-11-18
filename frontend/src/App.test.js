c3d188b66ab9332a05afb9fdeda1849c4f701b1b
annacraw05 <annacrawford2005@gmail.com>
2025-11-18 17:03:37 -0500

    added another frontend test and a few comments

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,8 +4,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-
-
+//this test checks that the website is launching with the correct message displayed
 test("renders the website greeting correctly", () => {
   render(<App />);
   const titleElement = screen.getByText(/Welcome Back! Log In to TigerTix:/i);
@@ -13,8 +12,16 @@ test("renders the website greeting correctly", () => {
 });
 
 //in order to run this test, you need to launch the backend servers and the front end webpage!!
-test("renders login prompt", async () => {
+//checks for register prompt
+test("renders register prompt", async () => {
   render(<App />);
   const eventElement = await screen.findByText(/Register!/i);
+  expect(eventElement).toBeInTheDocument();
+});
+
+//checks for the login prompt
+test("renders login prompt", async () => {
+  render(<App />);
+  const eventElement = await screen.findByText(/Login!/i);
   expect(eventElement).toBeInTheDocument();
 });
