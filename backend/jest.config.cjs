31524cd1992f0509b7bad15ce8490ceeab1bbfe2
annacraw05 <annacrawford2005@gmail.com>
2025-11-18 14:37:29 -0500

    trying to delete some of the old testing framework files, bye bye jest, hello vitest!

--- backend/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//   transform: {
-//     '^.+\\.jsx?$': ['babel-jest', { configFile: './babel.config.cjs' }]
-//   },
-//   testEnvironment: 'node',
-//   transformIgnorePatterns: ['/node_modules/']
-// };
