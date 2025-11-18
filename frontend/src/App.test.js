e5d0fd64be99cf79f568ada3d73aa37f1a3a8609
annacraw05 <annacrawford2005@gmail.com>
2025-11-18 14:50:38 -0500

    got some tests working for the frontend/updated them to work with vitest

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,17 +1,20 @@
+process.env.NODE_ENV = "test";
+
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
 
-test("renders the website title correctly", () => {
+
+test("renders the website greeting correctly", () => {
   render(<App />);
-  const titleElement = screen.getByText(/Welcome to Tiger Tix!/i);
+  const titleElement = screen.getByText(/Welcome Back! Log In to TigerTix:/i);
   expect(titleElement).toBeInTheDocument();
 });
 
 //in order to run this test, you need to launch the backend servers and the front end webpage!!
-test("renders an event name on the page", async () => {
+test("renders login prompt", async () => {
   render(<App />);
-  const eventElement = await screen.findByText(/Fall Career Expo/i);
+  const eventElement = await screen.findByText(/Register!/i);
   expect(eventElement).toBeInTheDocument();
 });
